788dd5c5fe52c807bdb93511b76e99474683ee10
parni-handayani <nikparni19@gmail.com>
2023-09-29 10:58:26 +0700

    part-8.2

---
+++ part-8.2/soal2.js
@@ -0,0 +1,63 @@
+let input = ["0001", "Roman Alamsyah ", "Bandar Lampung", "21/05/1989", "Membaca"];
+
+function dataHandling(data) {
+    data.splice(1,4,"Roman Alamsyah Elsharawy", "Provinsi Bandar Lampung", "21/05/1989", "Pria", "SMA Internasional Metro");
+    console.log(data);
+
+    let tanggal = data[3].split("/");
+    let bulan = tanggal[1];
+    switch(tanggal[1]){
+        case "01":
+            bulan = "Januari";
+            break;
+        case "02":
+            bulan = "Februari";
+            break;
+        case "03":
+            bulan = "Maret";
+            break;
+        case "04":
+            bulan = "April";
+            break;
+        case "05":
+            bulan = "Mei";
+            break;
+        case "06":
+            bulan = "Juni";
+            break;
+        case "07":
+            bulan = "Juli";
+            break;
+        case "08":
+            bulan = "Agustus";
+            break;
+        case "09":
+            bulan = "September";
+            break;
+        case "10":
+            bulan = "Oktober";
+            break;
+        case "11":
+            bulan = "November";
+            break;
+        case "12":
+            bulan = "Desember";
+            break;
+    }
+    console.log(bulan);
+    console.log(tanggal);
+    let tanggal3 = `${tanggal[0]}-${tanggal[1]}-${tanggal[2]}`;
+    console.log(tanggal3);
+}
+
+dataHandling(input);
+
+/**
+ * keluaran yang diharapkan (pada console)
+ *
+ * ["0001", "Roman Alamsyah Elsharawy", "Provinsi Bandar Lampung", "21/05/1989", "Pria", "SMA Internasional Metro"]
+ * Mei
+ * ["1989", "21", "05"]
+ * 21-05-1989
+ * Roman Alamsyah //batasi hanya 15 karakter saja pada array elemen ke 2
+ */
