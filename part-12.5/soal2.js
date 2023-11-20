c7ddd436ff1f5040d6db27b6cd41044cfc7d46e0
parni-handayani <nikparni19@gmail.com>
2023-11-20 10:55:12 +0700

    part-12.5 regex saol1&2

---
+++ part-12.5/soal2.js
@@ -0,0 +1,17 @@
+/*
+Diberikan sebuah function cariPelaku(kalimat) yang menerima satu parameter berupa string. 
+Function akan me-return jumlah berapa kali ditemukan kata "abc" secara berturut-turut di dalam kalimat tersebut. 
+Gunakan regex untuk melatih kemampuan regex.
+*/
+function cariPelaku(str) {
+  let myRegex = /abc+/ig;
+  let temp = str.match(myRegex);
+  return temp.length;
+}
+
+// TEST CASES
+console.log(cariPelaku('mabcvabc')); // 2
+console.log(cariPelaku('abcdabdc')); // 1
+console.log(cariPelaku('bcabcac')); // 1
+console.log(cariPelaku('abcabcabc')); // 3
+console.log(cariPelaku('babcbacabc')); // 2
